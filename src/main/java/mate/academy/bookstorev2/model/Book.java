05aac170643f22a60963c71becfb1bb9945120fa
Bohdan Shevtsov <shevtsov.b@gmail.com>
2024-04-24 19:03:39 +0300

    Book: Lombok annotations have changed

--- src/main/java/mate/academy/bookstorev2/model/Book.java
+++ src/main/java/mate/academy/bookstorev2/model/Book.java
@@ -6,10 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.math.BigDecimal;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
