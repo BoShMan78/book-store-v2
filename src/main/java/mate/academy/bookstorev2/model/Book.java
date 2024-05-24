e2c416239308728eec106664775677f0962c8751
Bohdan Shevtsov <shevtsov.b@gmail.com>
2024-05-24 11:47:26 +0300

    Repo, Service, Controller were created with methods getAll, createBook, getBookById

--- src/main/java/mate/academy/bookstorev2/model/Book.java
+++ src/main/java/mate/academy/bookstorev2/model/Book.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,17 +15,15 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
     private String title;
-    @Column(nullable = false)
     private String author;
-    @Column(nullable = false)
     private BigDecimal price;
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String isbn;
     private String description;
     private String coverImage;
--- src/main/java/mate/academy/bookstorev2/service/BookServiceImpl.java
+++ src/main/java/mate/academy/bookstorev2/service/BookServiceImpl.java
@@ -1,27 +1,39 @@
 package mate.academy.bookstorev2.service;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import mate.academy.bookstorev2.dto.BookDto;
+import mate.academy.bookstorev2.dto.CreateBookRequestDto;
+import mate.academy.bookstorev2.exception.EntityNotFoundException;
+import mate.academy.bookstorev2.mapper.BookMapper;
 import mate.academy.bookstorev2.model.Book;
 import mate.academy.bookstorev2.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    @Override
+    public BookDto save(CreateBookRequestDto requestDto) {
+        Book book = bookMapper.toModel(requestDto);
+        return bookMapper.toDto(bookRepository.save(book));
     }
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toDto)
+                .toList();
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public BookDto findBookById(Long id) {
+        Book book = bookRepository.findBookById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find book by id:" + id)
+        );
+        return bookMapper.toDto(book);
     }
 }
