ae28df0f4cd6b10d2bfa572928038488df6dd486
Bohdan Shevtsov <shevtsov.b@gmail.com>
2024-05-24 11:47:26 +0300

    Repo, Service, Controller were created with methods getAll, createBook, getBookById

---
+++ src/main/java/mate/academy/bookstorev2/dto/CreateBookRequestDto.java
@@ -0,0 +1,16 @@
+package mate.academy.bookstorev2.dto;
+
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateBookRequestDto {
+    private String title;
+    private String author;
+    private BigDecimal price;
+    private String isbn;
+    private String description;
+    private String coverImage;
+}
--- src/main/java/mate/academy/bookstorev2/service/BookService.java
+++ src/main/java/mate/academy/bookstorev2/service/BookService.java
@@ -1,10 +1,13 @@
 package mate.academy.bookstorev2.service;
 
 import java.util.List;
-import mate.academy.bookstorev2.model.Book;
+import mate.academy.bookstorev2.dto.BookDto;
+import mate.academy.bookstorev2.dto.CreateBookRequestDto;
 
 public interface BookService {
-    Book save(Book book);
+    BookDto save(CreateBookRequestDto requestDto);
 
-    List<Book> findAll();
+    List<BookDto> findAll();
+
+    BookDto findBookById(Long id);
 }
