64fc2c5e08e7af57aa5530545e446043be8409fb
Bohdan Shevtsov <shevtsov.b@gmail.com>
2024-06-06 11:00:14 +0300

    all liquibase bugs were fixed

--- src/main/java/mate/academy/bookstorev2/BookStoreV2Application.java
+++ src/main/java/mate/academy/bookstorev2/BookStoreV2Application.java
@@ -1,13 +1,9 @@
 package mate.academy.bookstorev2;
 
-import java.math.BigDecimal;
-import mate.academy.bookstorev2.model.Book;
 import mate.academy.bookstorev2.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreV2Application {
@@ -16,35 +12,5 @@ public class BookStoreV2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(BookStoreV2Application.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book bookAlice = new Book();
-            bookAlice.setAuthor("Luis Carol");
-            bookAlice.setTitle("Alice in Wonderland");
-            bookAlice.setDescription("""
-                    The famous Alice from Wonderland" is a little girl,
-                    about seven years old, with straight bangs and hair 
-                    that always climbs into her eyes.
-                    """);
-            bookAlice.setPrice(BigDecimal.valueOf(10));
-            bookAlice.setIsbn("978-1-2345-678-9-0");
-
-            Book bookJava = new Book();
-            bookJava.setAuthor("Allen B. Downey, Chris Mayfield");
-            bookJava.setTitle("Think Java: How to Think Like a Computer Scientist");
-            bookJava.setDescription("""
-                    Think Java is a hands-on introduction to computer science and 
-                    programming used by many universities and high schools around the world.
-                    """);
-            bookJava.setPrice(BigDecimal.valueOf(20));
-            bookJava.setIsbn("978-123-4567-89-0");
-
-            bookService.save(bookAlice);
-            bookService.save(bookJava);
-            System.out.println(bookService.findAll());
-        };
     }
 }
--- src/main/java/mate/academy/bookstorev2/model/Book.java
+++ src/main/java/mate/academy/bookstorev2/model/Book.java
@@ -10,23 +10,27 @@ import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@SQLDelete(sql = "UPDATE books SET is_deleted=true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
     private String title;
-    @Column(nullable = false)
     private String author;
-    @Column(nullable = false)
     private BigDecimal price;
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String isbn;
     private String description;
     private String coverImage;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
