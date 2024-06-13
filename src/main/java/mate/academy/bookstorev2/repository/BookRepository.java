5f227170294f4e46cc25a002766d81b6cbbd9789
Bohdan Shevtsov <shevtsov.b@gmail.com>
2024-06-13 14:17:50 +0300

    SearchByTitleAndAuthor was added

--- src/main/java/mate/academy/bookstorev2/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.bookstorev2.repository;
-
-import mate.academy.bookstorev2.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
