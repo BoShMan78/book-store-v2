23d353f2479a4f7da5d748fafb08afe896893743
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2024-06-13 14:19:52 +0300

    Spring data jpa hw 2 (#9)
    * All test have passed
    
    * Book: Lombok annotations have changed
    
    * There is an error in liquibase
    
    * Fixed all bugs with liquibase
    
    * dependencies were updated

--- src/main/java/mate/academy/bookstorev2/model/Book.java
+++ src/main/java/mate/academy/bookstorev2/model/Book.java
@@ -10,11 +10,15 @@ import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@SQLDelete(sql = "UPDATE books SET is_deleted=true WHERE id=?")
+@Where(clause = "is_deleted=false")
 @Table(name = "books")
 public class Book {
     @Id
@@ -27,4 +31,6 @@ public class Book {
     private String isbn;
     private String description;
     private String coverImage;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
