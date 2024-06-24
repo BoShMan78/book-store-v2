39f3eebf94c0e558f2c29654fa7912411a949e9f
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2024-06-24 11:23:04 +0300

    Pagination sorting swagger 2 (#14)
    * Pagination and Sorting were added
    
    * Swagger annotations and dependencies were added
    
    * After cherry pick with validation branch
    
    * Unused interface PagingAndSortingRepository was deleted.
    
    BookController: some extra validation and pagination annotations were added
    
    Pom.xml:spring-boot-starter-validation version was deleted

--- src/main/java/mate/academy/bookstorev2/controller/BookController.java
+++ src/main/java/mate/academy/bookstorev2/controller/BookController.java
@@ -1,13 +1,20 @@
 package mate.academy.bookstorev2.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstorev2.dto.BookDto;
 import mate.academy.bookstorev2.dto.BookSearchParametersDto;
 import mate.academy.bookstorev2.dto.CreateBookRequestDto;
 import mate.academy.bookstorev2.service.BookService;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,6 +26,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book management", description = "Endpoints for managing books")
+@Validated
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/books")
@@ -26,34 +35,42 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.findAll();
+    @Operation(summary = "Get all books", description = "Get a list of all available books")
+    public List<BookDto> getAll(@ParameterObject @PageableDefault Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
     @PostMapping
+    @Operation(summary = "Create a new book", description = "Create a new book")
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
     @GetMapping("/{id}")
-        public BookDto getBookById(@PathVariable Long id) {
+    @Operation(summary = "Get book by id", description = "Get book by id")
+    public BookDto getBookById(@PathVariable @Positive Long id) {
         return bookService.findBookById(id);
     }
 
     @PutMapping("/{id}")
-    public BookDto updateBookById(@PathVariable Long id,
+    @Operation(summary = "Update book by id", description = "Update book by id")
+    public BookDto updateBookById(@PathVariable @Positive Long id,
                                   @RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.updateBookById(id, bookDto);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete book by id", description = "Delete book by id")
     @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Long id) {
+    public void deleteById(@PathVariable @Positive Long id) {
         bookService.deleteById(id);
     }
 
     @GetMapping("/search")
-    public List<BookDto> searchBooks(@ModelAttribute BookSearchParametersDto searchParameters) {
+    @Operation(summary = "Search book by title and author",
+            description = "Search book by title and author")
+    public List<BookDto> searchBooks(@ModelAttribute BookSearchParametersDto searchParameters,
+                                     @ParameterObject @PageableDefault Pageable pageable) {
         return bookService.searchBooks(searchParameters);
     }
 }
