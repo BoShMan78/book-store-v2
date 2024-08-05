5893ddabe811cf604c423451b3cf713508fed159
Bohdan Shevtsov <shevtsov.b@gmail.com>
2024-08-05 17:49:10 +0300

    JwtAuthenticationFilter, OpenApiConfig: magic strings and number were replaced to constants

--- src/main/java/mate/academy/bookstorev2/security/JwtAuthenticationFilter.java
+++ src/main/java/mate/academy/bookstorev2/security/JwtAuthenticationFilter.java
@@ -18,6 +18,9 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer";
+    public static final int TOKEN_INDEX = 7;
     private final UserDetailsService userDetailsService;
     private final JwtUtil jwtUtil;
 
@@ -39,9 +42,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private String getToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
-            return bearerToken.substring(7);
+        String bearerToken = request.getHeader(AUTHORIZATION);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER)) {
+            return bearerToken.substring(TOKEN_INDEX);
         }
         return null;
     }
