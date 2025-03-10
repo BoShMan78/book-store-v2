02f8e087d9ecb16a30d8fb3c462817c9fecfc294
Bohdan Shevtsov <shevtsov.b@gmail.com>
2025-03-10 14:09:59 +0200

    liquibase sets changed. Deleted intermediate changes. Docker settings changed

--- src/main/java/mate/academy/bookstorev2/config/OpenApiConfig.java
+++ src/main/java/mate/academy/bookstorev2/config/OpenApiConfig.java
@@ -1,11 +1,7 @@
 package mate.academy.bookstorev2.config;
 
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Operation;
-import io.swagger.v3.oas.models.PathItem;
-import io.swagger.v3.oas.models.Paths;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
